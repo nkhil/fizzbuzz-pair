aaaaab3efa4dee355e8fdada6ffc60fc1296c1f2
Brooke Woolley <brooke.woolley@gmail.com>
2018-12-16 11:32:14 +0000

    Added third passing test

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,5 +1,7 @@
 def fizzbuzz(num)
-  if num % 3 == 0
+  if num % 3 == 0 and num % 5 == 0
+    'fizzbuzz'
+  elsif num % 3 == 0
     'fizz'
   else
     'buzz'
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -1,7 +1,11 @@
 require './lib/fizz_buzz'
 
 describe 'fizzbuzz' do
-  
+
+  it "returns 'fizzbuzz' when passed 15" do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+
   it "returns 'fizz' when passed 3" do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
@@ -9,6 +13,5 @@ describe 'fizzbuzz' do
   it "returns 'buzz' when passed 5" do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
-
 
 end
